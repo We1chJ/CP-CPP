de2a65d1942d6258c59d4aefbed73b9af567f9af
Jack Wei <jack_weq@icloud.com>
2022-07-18 21:30:40 -0400

    update

--- Codeforces/#807_div2/C.cpp
+++ Codeforces/#807_div2/C.cpp
@@ -9,7 +9,33 @@ int main(){
     int t;
     cin >> t;
     for(int _ = 0; _ < t; _++){
+        int n, c, q;
+        cin >> n >> c >> q;
+        string s;
+        cin >> s;
+        vector<ll> left(c+1), right(c+1), dif(c+1);
+        left[0] = 0;
+        right[0] = n;
 
+        for(int i = 1; i <= c; i ++){
+            ll a, b;
+            cin >> a >> b;
+            a--; b--;
+            left[i] = right[i-1];
+            right[i] = left[i] + b - a +1;
+            dif[i] = left[i] - a;
+        }
+
+        while(q--){
+            ll k;
+            cin >> k;
+            k--;
+            for(int i = c; i >= 1; i--){
+                if(k < left[i]) continue;
+                else k -= dif[i];
+            }
+            cout << s[k] << endl;
+        }
         
     } 
 }
