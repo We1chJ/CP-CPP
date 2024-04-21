ef8849a2ad5d7052171ae1a3a71c276fe1e71927
Jack Wei <jack_weq@icloud.com>
2024-04-21 09:46:49 -0400

    quick add before div 2

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include "bits/stdc++.h"
 
 using namespace std;
-typedef vector<int> vi;
-vi v (10);
 
 int main(){
     cin.tie(0)->sync_with_stdio(0);
     
-    cout << v[0] << "\n";
+    cout << "ss" << endl;
 }
