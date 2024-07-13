ff1a501de38173d23530ff005078d79e5bb79609
Jack Wei <jack_weq@icloud.com>
2024-07-12 23:14:53 -0400

    redone

--- CSES/MessageRoute.cpp
+++ CSES/MessageRoute.cpp
@@ -1,57 +1,55 @@
 #include "bits/stdc++.h"
+
 using namespace std;
-vector<int> adj[100001];
-vector<int> dis(100001, INT_MAX);
-int parent[100001];
+
+
 int main(){
     cin.tie(0)->sync_with_stdio(0);
 
     int n, m;
     cin >> n >> m;
-    for(int i =0 ; i < m; i++){
+    vector<int> adj[n+1];
+    for(int i = 1; i <= m; i++){
         int a, b;
         cin >> a >> b;
         adj[a].push_back(b);
         adj[b].push_back(a);
     }
+
+    vector<int> dist(n+1, INT_MAX);
+    vector<int> par(n+1, -1);
+
+    dist[1] = 0;
     queue<int> q;
     q.push(1);
-    dis[1] = 0;
-
     while(!q.empty()){
         int cur = q.front();
         q.pop();
-        for(auto a : adj[cur]){
-            if(dis[a] == INT_MAX){
-                dis[a] = dis[cur] + 1;
-                parent[a] = cur;
+        for(auto &a : adj[cur]){
+            if(dist[a] == INT_MAX){
                 q.push(a);
+                dist[a] = dist[cur] + 1;
+                par[a] = cur;
             }
         }
     }
 
-    if(dis[n] == INT_MAX)
-        cout << "IMPOSSIBLE" << endl;
-    else{
-        cout << dis[n]+1 << endl;
-        vector<int> v;
-        int cnt = dis[n];
-        v.push_back(n);
+    if(dist[n] == INT_MAX){
+        cout << "IMPOSSIBLE\n";
+    }else{
+        vector<int> ans;
         int cur = n;
-        while(cnt != 0){
-            for(auto a : adj[cur]){
-                if(dis[a] == cnt -1){
-                    cur = a;
-                    break;
-                }
-            }
-            v.push_back(cur);
-            cnt--;
+        while(par[cur] != -1){
+            ans.push_back(cur);
+            cur = par[cur];
         }
-        reverse(v.begin(), v.end());
-        for(auto a: v)
-            cout << a << " " ;
-        cout << endl;
+        ans.push_back(1);
+        reverse(ans.begin(), ans.end());
+        cout << ans.size() << endl;
+        for(auto &a : ans){
+            cout << a << " ";
+        }
+        cout << "\n";
     }
-    
+
 }
