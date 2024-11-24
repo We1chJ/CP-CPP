49123e729bac82d857d89809fb522e7c55b4df4e
Welch <weq511762338@gmail.com>
2024-11-24 12:52:22 -0500

    added subordinates tree algo revisit

--- CSES/Subordinates.cpp
+++ CSES/Subordinates.cpp
@@ -1,37 +1,75 @@
+// #include "bits/stdc++.h"
+
+// using namespace std;
+
+// vector<int> arr(200005);
+// vector<int> adj[200005];
+
+// void dfs(int cur, int par){
+//     for(int & a: adj[cur]){
+//         if(a != par)
+//             dfs(a, cur);
+//     }
+
+//     arr[par] += (arr[cur] + 1);
+// }
+
+
+// int main(){
+//     cin.tie(0)->sync_with_stdio(0);
+
+//     int n ;
+//     cin >> n;
+
+//     for(int i = 2; i <= n ;i ++){
+//         int a;
+//         cin >> a;
+//         // adj[i].push_back(a);
+//         adj[a].push_back(i);
+//     }
+
+//     dfs(1, 0);
+//     for(int i = 1; i <= n; i++){
+//         cout << arr[i];
+//         if(i != n) cout << " ";
+//     }
+//     cout << "\n";
+// }
+
+// Attemp on 11/23/2024
+
 #include "bits/stdc++.h"
 
 using namespace std;
 
-vector<int> arr(200005);
 vector<int> adj[200005];
+vector<int> cnt(200005);
 
-void dfs(int cur, int par){
-    for(int & a: adj[cur]){
-        if(a != par)
-            dfs(a, cur);
+void dfs(int p, int cur){
+    for(auto &a : adj[cur]){
+        if(a != p){
+            dfs(cur, a);
+        }
     }
-
-    arr[par] += (arr[cur] + 1);
+    cnt[p] += cnt[cur] + 1;
 }
-
 
 int main(){
     cin.tie(0)->sync_with_stdio(0);
 
-    int n ;
+    int n =0 ;
     cin >> n;
-
-    for(int i = 2; i <= n ;i ++){
+    for(int i = 2 ; i <= n; i++){
         int a;
         cin >> a;
-        // adj[i].push_back(a);
         adj[a].push_back(i);
+        adj[i].push_back(a);
     }
 
-    dfs(1, 0);
-    for(int i = 1; i <= n; i++){
-        cout << arr[i];
-        if(i != n) cout << " ";
+    dfs(0, 1);
+    for(int i = 1; i <=n ;i ++){
+        cout << cnt[i] << " ";
     }
-    cout << "\n";
+    cout << endl;
+
 }
