f594cacef2276ef88230824e5dd6a4ee5a4f23bc
Welch <weq511762338@gmail.com>
2024-12-06 20:57:22 -0500

    ccc prep for ktbyte

---
+++ CCC/JuniorDiv/Deliv-e-droid/Main.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        int p = sc.nextInt();
+        int c = sc.nextInt();
+        sc.close();
+
+        System.out.println((p*50 - c * 10) + ((p > c) ? 500 : 0));
+    }    
+}
