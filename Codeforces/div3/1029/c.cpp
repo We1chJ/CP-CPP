f44a7847aea7de10e2bcdcc4d87a35e99ceeb23b
Welch <weq511762338@gmail.com>
2025-06-14 00:00:24 -0400

    somehow set is moe efficient than unordered set

--- Codeforces/div3/1029/c.cpp
+++ Codeforces/div3/1029/c.cpp
@@ -1,57 +1,42 @@
 #include "bits/stdc++.h"
 
 using namespace std;
-using ll = long long;
 
 int main(){
+    cin.tie(0) -> sync_with_stdio(0);
     int t;
     cin >> t;
     while(t--){
         int n;
         cin >> n;
-        vector<int> v(n);
-        
-        vector<int> freq (n+1, 0);
-        for(int i = 0; i < n; i++){
+        vector<int> v (n);
+        vector<int> cnt(n+1);
+        cnt[0] = 0;
+        set<int> s;
+        for(int i = 0 ; i <n ; i++){
             cin >> v[i];
-            freq[v[i]] ++;
+            s.insert(v[i]);
+            cnt[i+1] = s.size();
         }
 
-        int l = 0, r = n;
-        while(l < r){
-            int mid = (l+r+1)/2;
-            int segCnt = 0;
-            vector<int> f = freq;
-            set<int> prev;
-            set<int> cur;
-            bool success = true;
-            for(int i = 0; i< n; i++){
-                if(segCnt >= mid){
-                    success = false;
-                    break;
-                }
-                if(i != 0 && f[v[i]] < mid - segCnt){
-                    if (!includes(cur.begin(), cur.end(), prev.begin(), prev.end()) || cur.size() < prev.size()){
-                        success = false;
-                        break;
-                    }
-                    prev = cur;
-                    cur.clear();
-                    segCnt ++; //switch 
-                }
-                cur.insert(v[i]);
-                f[v[i]]--;
-            }
-            if (!includes(cur.begin(), cur.end(), prev.begin(), prev.end()) || cur.size() < prev.size()){
-                success = false;
-            }
-
-            if(success){
-                l = mid;
-            }else{
-                r = mid-1;
-            }
+        if(n == 1) {
+            cout << 1 << endl;
+            continue;
         }
-        cout << l << endl;        
+        int ans = 0;
+
+        int r = n;
+        int l = r;
+        while(l > 0 && r > 0){
+            s.clear();
+            while(l > 0 && s.size() != cnt[r]){
+                s.insert(v[l-1]);
+                l--;
+            }
+            ans++;
+            r = l;
+        }
+
+        cout << ans << endl;
     }
 }
