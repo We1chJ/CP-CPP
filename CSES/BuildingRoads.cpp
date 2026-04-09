8960e11246e5d00287e59d5e33c3e218377f4915
Welch <weq511762338@gmail.com>
2026-04-08 21:40:59 -0400

    add new

--- CSES/BuildingRoads.cpp
+++ CSES/BuildingRoads.cpp
@@ -1,47 +1,50 @@
 #include "bits/stdc++.h"
 
 using namespace std;
-bool visited[100005];
-vector<int> adj[100005];
+
+vector<int> adj[100001];
+bool visited[100001]; // set default values 0 (false)
 
 void dfs(int cur){
-    if(visited[cur])
-        return;
+    if(visited[cur]) return;
     visited[cur] = true;
-    for(auto & a: adj[cur]){
-        dfs(a);
-    }
+    
+    for(auto next : adj[cur]){
+        dfs(next);
+    }   
 }
 
-
-int main() {
-    cin.tie(0)->sync_with_stdio(0);
-
+int main(){
     int n, m;
     cin >> n >> m;
-    
-    for(int i= 0; i < m; i++){
+
+    for(int i  = 0; i < m; i++){
         int a, b;
         cin >> a >> b;
         adj[a].push_back(b);
         adj[b].push_back(a);
     }
+
+    // count number of components
     int cnt = 0;
-    vector<int> ans;
-    for(int i = 1; i <= n; i++){
+
+    vector<int> nodes;
+
+    // try every single node
+    for(int i = 1; i<=n;i++){
+        // only start a traversal on the node if its not visited yet
         if(!visited[i]){
-            cnt++;
-            ans.push_back(i);
+            nodes.push_back(i);
+            // traverse over the whole components
             dfs(i);
+            cnt ++;
         }
     }
 
-    cout << cnt - 1 << endl;
-    for(int i = 0 ; i < ans.size()-1; i++){
-        cout << ans[i] << " " << ans[i+1] << endl;
+    cout << cnt -1  << endl;
+
+    for(int i = 1; i< nodes.size(); i++){
+        cout << nodes[0] << " " << nodes[i] << endl;
     }
 
-
-    
-    
 }
--- CSES/BuildingTeams.cpp
+++ CSES/BuildingTeams.cpp
@@ -1,46 +1,48 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-vector<int> adj[100005];
-int arr[100005];
-bool fail;
-void dfs(int cur, int color){
-    if(arr[cur] != 0){
-        if(arr[cur] != color){
-            fail = true;
-        }            
-        return ;
-    }
+vector<int> adj[100001];
+bool visited[100001];
+int colors[100001]; // defaulted to 0
+bool isPossible = true;
 
-    arr[cur] = color;
-    for(auto & a : adj[cur]){
-        dfs(a, (color == 1) ? 2 : 1);
+void dfs(int cur, int color){
+    if(visited[cur]) return;
+    visited[cur] = true;
+    colors[cur] = color;
+    for(auto a : adj[cur]){
+        // report impossible if neighboring nodes share same color
+        if(colors[a] == colors[cur]) {
+            isPossible = false;
+            return ;
+        }
+        dfs(a, 3 - color);
     }
 }
 
 int main(){
-    cin.tie(0)->sync_with_stdio(0);
     int n, m;
-    cin >> n >> m;
-    for(int i  =0 ; i < m; i++){
+    cin >> n >>m;
+
+    for(int i = 0; i < m; i++){
         int a, b;
-        cin >> a >> b;
+        cin >> a>> b;
         adj[a].push_back(b);
         adj[b].push_back(a);
     }
 
-    for(int i  =1; i <= n; i++){
-        if(arr[i] == 0){
+    for(int i = 1; i<= n;i++){
+        if(!visited[i]){
             dfs(i, 1);
         }
     }
 
-    if(fail) cout << "IMPOSSIBLE" << endl;
-    else{
-        for(int i  = 1; i <= n; i++){
-            cout << arr[i] << " " ;
+    if(!isPossible){
+        cout << "IMPOSSIBLE" << endl;
+    }else{
+        for(int i = 1; i <= n;i ++){
+            cout << colors[i] << " ";
         }
-        cout << "\n";
+        cout << endl;
     }
-
 }
